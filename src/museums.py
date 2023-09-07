f4e6b1b6218c1360a8cc0d7bdc88ba4a8ab2363b
Bartosz Balazinski <bart.balaz@gmail.com>
2023-09-07 13:19:25 -0400

    per

--- src/museums.py
+++ src/museums.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import io
-import math
+import re
 
 class InvalidContentFromWikipedia(Exception):
     pass
@@ -41,31 +41,32 @@ def _download_table(page: str, section: int) -> DataFrame:
     if response_df is None:
         raise InvalidContentFromWikipedia
 
-    print('4 Success')
+    print('Done')
     return response_df
 
 
-def _get_location_values(location: str) -> Tuple[str, str]:
+def _get_location_values(location: str, custom_locations: bool) -> Tuple[str, str]:
 
-    location_city, location_country = location_overrides.get(location, (None, None))
+    location_city, location_country = location_overrides.get(location, (None, None)) if custom_locations else (None,None)
 
-    if location_city is not None:
-        return (location_city, location_country)
+    if not location_city:
+        location_list = location.split(',')
 
-    location_list = location.split(',')
+        while len(location_list) < 2:
+            location_list.append(None)
 
-    while len(location_list) < 2:
-        location_list.append(None)
+        while len(location_list) > 2:
+            del location_list[-1]
 
-    while len(location_list) > 2:
-        del location_list[-1]
+        location_city, location_country = (tuple(location_list))
 
-    return tuple(location_list)
+    return (location_city if location_city else '', location_country if location_country else '')
 
 
-def _get_population(city_name: str) -> int:
+def _get_population(city_name: str, custom_population: bool) -> int:
     val_df = city_data.query("city == @city_name")
-    return val_df['population'].item() if not val_df.empty else missing_city_populations.get('city_name', 0)
+    return val_df['population'].item() if not val_df.empty else missing_city_populations.get(city_name, 0) if custom_population else 0
+
 
 
 
@@ -82,26 +83,36 @@ def download_data():
     city_data = city_data.drop(city_data.iloc[:, 3:13], axis=1)
     city_data = city_data.drop(city_data.index[0])
     city_data.columns = ['city', 'country', 'population']
-    city_data = city_data.astype({'population': int})
+    city_data = city_data.astype({'city': str, 'country': str, 'population': int })
+    city_data = city_data.fillna(' ')
 
 
 
-def create_master_data():
+def create_master_data(custom_locations: bool=True, custom_population: bool=True):
     global master_data
 
+    print('Creating master data')
     master_data=DataFrame(data = { 'name': [], 'city': [], 'country': [], 'visitors': [], 'population': [] })
     for i, row in museum_data.iterrows():
         # Get the locations
-        location_city, location_country = _get_location_values(row['location'])
+        location_city, location_country = _get_location_values(row['location'], custom_locations)
         # Get the visitors value (get rid of the commans and the trailing garbage)
-        visitors = int(row['visitors'].split('[')[0].replace(',',''))
+        pattern = '^[0-9,]+'
+        match = re.match(pattern, row['visitors'])
+        visitors = int(match.group(0).replace(',','')) if match else 0
         # Get the city population
-        population = _get_population(location_city)
+        population = _get_population(location_city, custom_population)
         master_data.loc[len(master_data)] = [row['name'], location_city, location_country, visitors, population ]
-        print(f'{row["name"]}: {location_city}, {location_country}, {visitors}, {population}')
+        #print(f'{row["name"]}: {location_city}, {location_country}, {visitors}, {population}')
 
+
+def verify_master_data():
+    print('Verifying master data')
+    global master_data_issues
+    master_data_issues = master_data.query("visitors == 0 or population == 0 or country =='' or not country.str.match('^[A-Za-z\ ]*$')")          # or not country.str.match('^[A-Za-z]$')")
 
 if __name__ == '__main__':
     download_data()
-    create_master_data()
-    print()
+    create_master_data(False, False)
+    verify_master_data()
+    print(master_data_issues.to_markdown())
